8ed6583ee27403ac865751e57fe2160ae0c013ea
mysterybee07 <biraj2019pudasaini@gmail.com>
2024-06-14 00:01:54 +0545

    slices done

--- Day-07/slicepassbyreference.go
+++ Day-07/slicepassbyreference.go
@@ -6,11 +6,13 @@ func modifySlice(s []int) {
 	if len(s) > 0 {
 		s[0] = 100
 	}
-
+	fmt.Println("Original slice before passing", s)
 }
 
 func main() {
 	originalSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println("Original slice", originalSlice)
 
 	modifySlice(originalSlice)
 
