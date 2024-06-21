838f95b0ef58e3ca5f711f6beec414542d707408
mysterybee07 <biraj2019pudasaini@gmail.com>
2024-06-21 23:29:26 +0545

    secondlargest element in an array

--- Questionsanswers/secondLargest.go
+++ Questionsanswers/secondLargest.go
@@ -1,36 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func SecondLargest(arr []int) int {
-	largest := arr[0]
-	secondLargest := 0
 	if len(arr) < 2 {
+		fmt.Println("Array must contain at least two distinct elements")
 		return 0
 	}
-	if len(arr) == 2 {
-		if arr[0] < arr[1] {
-			secondLargest = arr[0]
-		} else {
-			secondLargest = arr[1]
-		}
 
-	} else {
-		for i, _ := range arr {
-			if largest > arr[i] {
-				secondLargest = largest
-				largest = arr[i]
-			} else if largest > arr[i] && secondLargest < arr[i] {
-				secondLargest = arr[i]
-			}
-		}
+	largest := math.MinInt32
+	secondLargest := math.MinInt32
 
+	for _, num := range arr {
+		if num > largest {
+			secondLargest = largest
+			largest = num
+		} else if num > secondLargest && num != largest {
+			secondLargest = num
+		}
 	}
+
+	if secondLargest == math.MinInt32 {
+		fmt.Println("No second largest element found")
+		return 0
+	}
+
 	return secondLargest
 }
 
 func main() {
-	arr := []int{22, 11}
-	secondLargest := SecondLargest(arr)
-	fmt.Println("Second largest element is: ", secondLargest)
+	arr1 := []int{20, 15, 17, 18, 19}
+	fmt.Println("Second largest element is:", SecondLargest(arr1))
+
+	arr2 := []int{10, 10, 10, 10, 10}
+	fmt.Println("Second largest element is:", SecondLargest(arr2))
 }
